74cb4cd9a5a7146975ca3d8ecf6290f614c729eb
Liam Lamont <142560958+asce-ljl23@users.noreply.github.com>
2023-10-09 11:46:51 +0100

    Added smooth_image function and created example

--- scripts/smooth_image.py
+++ scripts/smooth_image.py
@@ -1,5 +1,19 @@
-from envtest import rand_array
+from envtest import rand_array, smooth_image
+from scipy import misc
+import matplotlib.pyplot as plt
 
 shape = (3, 3)
 
 print(rand_array(shape))
+
+image = misc.ascent()
+sigma = 5
+
+smoothed_image = smooth_image(image, sigma)
+
+f = plt.figure()
+f.add_subplot(1,2,1)
+plt.imshow(image)
+f.add_subplot(1,2,2)
+plt.imshow(smooth_image(smoothed_image))
+plt.show()
